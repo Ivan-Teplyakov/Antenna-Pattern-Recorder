c4d30da2984254acb2cdcac1dc41d68d6aa24011
Oleksii Liske <58223178+liske2017@users.noreply.github.com>
2020-01-10 23:54:27 +0200

    Add files via upload

---
+++ Arduino_Project/AntennaPatternRecorder/Data_Recorder.py
@@ -0,0 +1,29 @@
+import serial
+import time
+ser = serial.Serial('COM5', 9600)
+time.sleep(2)
+ser.write(b'1')                # change from 1 to 2 to rotate at both sides
+# Read and record the data
+data =[]                       # empty list to store the data
+for i in range(155):           # number of degrees
+    b = ser.readline()         # read a byte string
+    string_n = b.decode()  # decode byte string into Unicode  
+    string = string_n.rstrip() # remove \n and \r
+    flt = float(string)        # convert string to float
+    print(flt)
+    data.append(flt)           # add to the end of data list
+   
+ser.close()
+# show the data
+
+for line in data:
+    print(line)
+
+    import matplotlib.pyplot as plt
+# if using a Jupyter notebook include %matplotlib inline
+
+plt.plot(data)
+plt.xlabel('Time (seconds)')
+plt.ylabel('Potentiometer Reading')
+plt.title('Potentiometer Reading vs. Time')
+plt.show()
